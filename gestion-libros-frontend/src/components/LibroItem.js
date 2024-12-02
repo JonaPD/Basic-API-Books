202f2a30f6459a8eb53bec025f9b4dc4503a96bb
swJhonws <davidpalan621@gmail.com>
2024-12-02 19:29:49 +0100

    Cambios de estilo de texto

--- gestion-libros-frontend/src/components/LibroItem.js
+++ gestion-libros-frontend/src/components/LibroItem.js
@@ -5,10 +5,10 @@ const LibroItem = ({ libro }) => {
   return (
     <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
       <h3>{libro.title}</h3>
-      <p>Autor: {libro.author}</p>
-      <p>Género: {libro.genre}</p>
-      <p>Año: {libro.publication_year}</p>
-      <p>Estado: {libro.status}</p>
+      <p><b>Autor:</b> {libro.author}</p>
+      <p><b>Género:</b> {libro.genre}</p>
+      <p><b>Año:</b> {libro.publication_year}</p>
+      <p><b>Estado:</b> {libro.status}</p>
     </div>
   );
 };
